d7260d235f762418ec9809834c28da23a424a9a9
caobinxin <caobinxin666@163.com>
2019-10-26 20:09:37 +0800

    add gate.c
    Signed-off-by: caobinxin <caobinxin666@163.com>

---
+++ x86-64os-source-code/os_code/chapter_2_primary/第4章/程序/程序4-9/kernel/gate.c
@@ -0,0 +1,32 @@
+#include "gate.h"
+  void set_intr_gate(unsigned int n,unsigned char ist,void * addr)
+{
+	_set_gate(IDT_Table + n , 0x8E , ist , addr);	//P,DPL=0,TYPE=E
+}
+
+/*
+
+*/
+
+  void set_trap_gate(unsigned int n,unsigned char ist,void * addr)
+{
+	_set_gate(IDT_Table + n , 0x8F , ist , addr);	//P,DPL=0,TYPE=F
+}
+
+/*
+
+*/
+
+  void set_system_gate(unsigned int n,unsigned char ist,void * addr)
+{
+	_set_gate(IDT_Table + n , 0xEF , ist , addr);	//P,DPL=3,TYPE=F
+}
+
+/*
+
+*/
+
+  void set_system_intr_gate(unsigned int n,unsigned char ist,void * addr)	//int3
+{
+	_set_gate(IDT_Table + n , 0xEE , ist , addr);	//P,DPL=3,TYPE=E
+}
--- x86-64os-source-code/os_code/chapter_2_primary/第4章/程序/程序4-9/kernel/gate.h
+++ x86-64os-source-code/os_code/chapter_2_primary/第4章/程序/程序4-9/kernel/gate.h
@@ -75,37 +75,6 @@ do{									\
 
 */
 
-inline void set_intr_gate(unsigned int n,unsigned char ist,void * addr)
-{
-	_set_gate(IDT_Table + n , 0x8E , ist , addr);	//P,DPL=0,TYPE=E
-}
-
-/*
-
-*/
-
-inline void set_trap_gate(unsigned int n,unsigned char ist,void * addr)
-{
-	_set_gate(IDT_Table + n , 0x8F , ist , addr);	//P,DPL=0,TYPE=F
-}
-
-/*
-
-*/
-
-inline void set_system_gate(unsigned int n,unsigned char ist,void * addr)
-{
-	_set_gate(IDT_Table + n , 0xEF , ist , addr);	//P,DPL=3,TYPE=F
-}
-
-/*
-
-*/
-
-inline void set_system_intr_gate(unsigned int n,unsigned char ist,void * addr)	//int3
-{
-	_set_gate(IDT_Table + n , 0xEE , ist , addr);	//P,DPL=3,TYPE=E
-}
 
 
 /*
@@ -128,6 +97,10 @@ unsigned long ist4,unsigned long ist5,unsigned long ist6,unsigned long ist7)
 	*(unsigned long *)(TSS64_Table+21) = ist7;	
 }
 
+extern void set_intr_gate(unsigned int n,unsigned char ist,void * addr);
+extern void set_trap_gate(unsigned int n,unsigned char ist,void * addr);
+extern void set_system_gate(unsigned int n,unsigned char ist,void * addr);
+extern void set_system_intr_gate(unsigned int n,unsigned char ist,void * addr);	//int3
 
 #endif
 
